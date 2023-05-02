252e675b2684053333b7f38c7318fa0adcdd64b2
jooseariias <arijoose@gmail.com>
2023-05-02 20:38:58 -0300

    styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,11 +48,11 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className=" home-cont  h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-2xl sm:text-6xl font-bold text-white">
+          <h2 className=" H2-HOME text-2xl sm:text-6xl font-bold text-white">
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
@@ -78,7 +78,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        <div className="mt-36"  > 
           <img
             src={HeroImage}
             alt="my profile"
